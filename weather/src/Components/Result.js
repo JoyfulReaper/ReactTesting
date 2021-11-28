23833203f1292988454b190c7d953837b67d9e51
Kyle Givler <xvegan88x@gmail.com>
2021-11-28 13:45:16 -0500

    typo

--- weather/src/Components/Result.js
+++ weather/src/Components/Result.js
@@ -3,6 +3,10 @@ import React from 'react';
 const Result = ({data}) => {
 return (
     <div id="result" style={{display: "none"}}>
+    {
+        data.error !== undefined ? <h1 className="display-6">{data.error.message}</h1>
+        :
+        <>
         <h1 className="display-6">Current Weather</h1>
         <p>
             <strong>Location:</strong> {data.location.name}, {data.location.region}, {data.location.country}<br />
@@ -12,11 +16,13 @@ return (
             <strong>Wind:</strong> {data.current.wind_mph} MPH <br />
             <strong>Wind Gusts:</strong> {data.current.gust_mph} MPH <br />
             <strong>Wind Direction:</strong> {data.current.wind_dir} <br />
-            <strong>Humidity:</strong> {data.current.humidity} F <br />
+            <strong>Humidity:</strong> {data.current.humidity}% <br />
             <strong>Visibility:</strong> {data.current.vis_miles} miles <br />
             <strong>Cloud Coverage:</strong> {data.current.cloud}% <br />
             <strong>Percipitation:</strong> {data.current.precip_in} inches<br />
         </p>
+        </>
+    }
     </div>
 );
 }
